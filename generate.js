e9b88d927c64c6a4c04a71f12fb074dc37109c70
Ferwin Lopez <102341775+Fer-Win@users.noreply.github.com>
2024-01-14 00:06:37 +0530

    Add files via upload

---
+++ generate.js
@@ -0,0 +1,15 @@
+import fs from 'fs';
+
+const gen = fs.createWriteStream('generated.txt');
+
+
+for(let i = 1; i <= 100; i++){
+    gen.write(`case ${i}:\n`);
+    gen.write(`\tswitch(diceRoll){\n`);
+        for(let j =1; j<=6;j++){
+            gen.write(`\t\tcase ${j}:\n`);
+            gen.write(`\t\t\tposition = ${Math.ceil(Math.random()*100)};\n`);
+            gen.write(`\t\t\tbreak;\n`);
+        }
+    gen.write(`\t}\n`);
+}
